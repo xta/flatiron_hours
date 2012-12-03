3847f5cd639df5e698bc5d93e43708f45fa2674c
bradwheel <bradley.m.wheel@gmail.com>
2012-12-03 09:02:05 -0500

    fancybox finish

--- public/application.js
+++ public/application.js
@@ -3,7 +3,32 @@
 $(document).ready(function() {
   $(document).keyup(function(e) {
       if (e.keyCode == 70 ) {
-        $.fancybox.open("http://www.youtube.com/watch?v=UGS8re8cIVI&feature=fvst")
+
+        var url = "http://www.youtube.com/watch?v=UGS8re8cIVI&feature=fvst&autoplay=1";
+        var formattedURL = url.replace(new RegExp("watch\\?v=", "i"), 'v/');
+
+  $.fancybox({
+      'padding'   : 0,
+      'autoScale'   : false,
+      'transitionIn'  : 'none',
+      'transitionOut' : 'none',
+      'autoplay': true,
+      'title'     : this.title,
+      'width'   : 680,
+      'height'    : 495,
+      'href'      : formattedURL,
+      'type'      : 'swf',
+      'swf'     : {
+           'wmode'    : 'transparent',
+        'allowfullscreen' : 'true'
+      }
+    });
+
+  return false;
+
+
+
+
       }
   });
 });
